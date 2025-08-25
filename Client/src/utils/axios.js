a522bed0d44910b97ec8b8b43434546f82840cad
Himesh Ambaliya <158713503+Himesh565@users.noreply.github.com>
2025-08-25 12:23:53 +0530

    Create axios.js

---
+++ Client/src/utils/axios.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+// Set base URL for all API calls
+const instance = axios.create({
+  baseURL: 'http://localhost:5000',
+  timeout: 10000,
+});
+
+// Add request interceptor to include auth token
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor to handle errors
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance; 
